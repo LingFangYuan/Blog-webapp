90a64409e0a5258ed056e92b23dc5d8c69af3fd9
Ling <786173189@qq.com>
2019-05-29 17:50:20 +0800

    modified models.py

--- www/models.py
+++ www/models.py
@@ -27,7 +27,7 @@ class Blog(Model):
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
-    user_name = StringField(ddl='varchar(500)')
+    user_image = StringField(ddl='varchar(500)')
     name = StringField(ddl='varchar(50)')
     summary = StringField(ddl='varchar(200)')
     content = TextField()
@@ -35,7 +35,7 @@ class Blog(Model):
 
 
 class Comment(Model):
-    __table__ = 'comment'
+    __table__ = 'comments'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     blog_id = StringField(ddl='varchar(50)')
