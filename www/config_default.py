c4fc1da37ad082c8819612c876df02bc900da344
Ling <786173189@qq.com>
2019-06-04 11:41:11 +0800

    add config.py

---
+++ www/config_default.py
@@ -0,0 +1,13 @@
+
+configs = {
+    'db': {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'root',
+        'password': '111111',
+        'db': 'blogs'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
